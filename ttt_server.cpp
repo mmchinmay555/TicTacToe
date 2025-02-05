ad5cbb24f0f89b0dc2f7a59db872a5afe6267540
Chinmay Rao <chinmay.rao@zeiss.com>
2025-02-05 18:24:20 +0530

    Added Server and client for TicTacToe game specific

---
+++ ttt_server.cpp
@@ -0,0 +1,6 @@
+#include "ttt_server.h"
+
+TTT_Server::TTT_Server()
+{
+
+}
---
+++ ttt_server.h
@@ -0,0 +1,11 @@
+#ifndef TTT_SERVER_H
+#define TTT_SERVER_H
+
+
+class TTT_Server
+{
+public:
+    TTT_Server();
+};
+
+#endif // TTT_SERVER_H
